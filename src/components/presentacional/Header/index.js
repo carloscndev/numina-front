904e1f010528f43dc16d259b6f79689f5e50db62
carloscastanedan <carloscastanedan@gmail.com>
2020-01-27 14:21:59 -0600

    added: Header component

--- src/components/presentacional/Header/index.js
+++ src/components/presentacional/Header/index.js
@@ -1,9 +1,30 @@
 // Import Modules
 import React from 'react'
 
+// Import Componentes
+import HamburguerIcon from '../../icons/HamburgerIcon'
+
+// Import Styles
+import './styles.scss'
+
 const Header = () => (
   <header>
-    Header
+    <div className='header-container'>
+      <div className='first-section'>
+        <div>Logo</div>
+        <div>
+          <HamburguerIcon />
+        </div>
+      </div>
+      <div className='second-section'>
+        <div>
+          Notifications
+        </div>
+        <div>
+          User
+        </div>
+      </div>
+    </div>
   </header>
 )
 
