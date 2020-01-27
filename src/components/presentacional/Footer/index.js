d180ade0068516f61b5848b32d9df41d7099242a
carloscastanedan <carloscastanedan@gmail.com>
2020-01-27 14:40:49 -0600

    updated: footer

--- src/components/presentacional/Footer/index.js
+++ src/components/presentacional/Footer/index.js
@@ -1,9 +1,19 @@
 // Import Modules
 import React from 'react'
 
+// Import Styles
+import './styles.scss'
+
 const Footer = () => (
   <footer>
-    Footer
+    <div className='footer-container'>
+      <div className='contac'>
+        hola@numina.mx
+      </div>
+      <div className='legal'>
+        Numina ©2020 Todos los derechos reservados.
+      </div>
+    </div>
   </footer>
 )
 
