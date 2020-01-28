21dc1c550921004d65a35e2272edef77631eca4d
carloscastanedan <carloscastanedan@gmail.com>
2020-01-28 05:21:22 -0600

    updated: MainLayout add styles

--- src/components/layouts/MainLayout/index.js
+++ src/components/layouts/MainLayout/index.js
@@ -5,14 +5,17 @@ import React from 'react'
 import Header from '../../presentacional/Header'
 import Footer from '../../presentacional/Footer'
 
+// Import Styles
+import './styles.scss'
+
 const MainLayout = ({ children }) => (
-  <>
+  <div className='main-layout'>
     <Header />
     <main>
       {children}
     </main>
     <Footer />
-  </>
+  </div>
 )
 
 export default MainLayout
